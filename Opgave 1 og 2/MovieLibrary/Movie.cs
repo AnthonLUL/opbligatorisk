7aa51eb6e6c8d0e2f6a5148d9bdcf9bdd89056ce
AnthonLUL <90059850+AnthonLUL@users.noreply.github.com>
2023-01-03 19:01:03 +0100

    Add files via upload

---
+++ Opgave 1 og 2/MovieLibrary/Movie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public int LengthInMinutes { get; set; }
+        public string CountryOfOrigin { get; set; }
+
+        public Movie(int id, string movieName, int lengthInMinutes, string countryOfOrigin)
+        {
+            Id = id;
+            MovieName = movieName;
+            LengthInMinutes = lengthInMinutes;
+            CountryOfOrigin = countryOfOrigin;
+        }
+        public Movie()
+        {
+
+        }
+    }
+}
---
+++ Opgave 1 og 2/MovieLibrary/TimeCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLibrary
+{
+    public class TimeCalculator
+    {
+        public int ConvertMinutesToSeconds(int minutes)
+        {
+            if (minutes < 0)
+            {
+                throw new Exception("This method converts minutes to seconds. if the minutes are below 0 it is already converted to seconds");
+            }
+            return minutes * 60;
+        }
+    }
+}
